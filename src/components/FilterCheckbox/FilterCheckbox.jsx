89983aa5a1919fb59d221c31744a2bc2990baca1
Cristina <kristi.chichkanova@gmail.com>
2023-11-17 15:19:42 +0100

    layout landing_intro

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -3,11 +3,15 @@ import './FilterCheckbox.css'
 
 function FilterCheckbox( ){
     return (
-        
-        <label className="filter" htmlFor="short-films">
+
+        <label className="filter" htmlFor="short-films"> 
+        <div className='filter__container'>
         <input type="checkbox" className='filter__checkbox' id="short-films" name='short-films'/>
         <span className="filter__slider "></span>
+        </div>
         Короткометражки</label>
+        
+
         
     );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Main.css'
+import landingLogo from '../../images/landing-logo.png'
 
 function Main() {
     return (
-        <div>
-            <h2>компонент страницы «О проекте»</h2>
-        </div>
+        <main className="landing">
+            <section className="intro">
+               <div className='intro__wrapper'>
+               <img className='into__image' src={landingLogo} alt="Изображение глобуса с надписью web"></img>
+               <div className='intro__container'>
+               <h1 className="intro__title">Учебный проект студента факультета Веб-разработки.</h1> 
+               <p className="intro__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+               <a href="#aboutProject">
+               <button className="intro__button">Узнать больше</button>
+               </a>
+               </div>
+               </div> 
+            </section>
+            <section className="project" id='aboutProject'></section>
+            <section className="technologies"></section>
+            <section className="portflio"></section>
+        </main>
     );
 }
 
