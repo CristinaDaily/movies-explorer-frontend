7d59c590ccb197e31a21170ba5595a987ead8238
Cristina <kristi.chichkanova@gmail.com>
2023-12-01 14:33:19 +0200

    add cardDelete button for /saved-movie page

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React,  { useState, useEffect } from 'react';
+import {Link, useLocation} from 'react-router-dom';
 import movie1 from '../../images/movie1.png';
 import './MoviesCard.css';
 
 
+
 function MoviesCard({name, duration}) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const location = useLocation();
+
+  const moviePage = location.pathname === '/movies';
+  const savedMoviesPage =location.pathname ==='/saved-movies';
+
+  const handleMouseEnter = () => {
+    if (savedMoviesPage) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+
     return (
-        <article className='element'>
+        <article 
+        className='element'
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
           <img
             className='element__image'
             src={movie1}
@@ -13,10 +36,16 @@ function MoviesCard({name, duration}) {
           />
         <div className='element__wrapper'>
           <h2 className='element__movie-name'>{name}</h2>
-            <button
+          {moviePage && <button
               type='button'
               className='element__like-btn element__like-btn_active'
-            ></button>
+            ></button>}
+          {savedMoviesPage && (
+           <button
+              type='button'
+              className= {`element__deleteCard-btn ${isHovered &&'element__deleteCard-btn_active'}`}
+            ></button>)}  
+            
         </div>
         <div className='element__line'></div>
             <p className='element__duration'>{duration}</p>
